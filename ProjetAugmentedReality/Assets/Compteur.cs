399c0dde4e0dc37240eae6ea753193dc2b361049
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2026-04-22 09:30:34 +0200

    pas grand chose hein

---
+++ ProjetAugmentedReality/Assets/Compteur.cs
@@ -0,0 +1,49 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Compteur : MonoBehaviour
+{
+    private int _value = 0;
+    [SerializeField] private TextMeshProUGUI text;
+
+
+    void Start()
+    {
+        UpdateValue();
+    }
+
+    public void UpdateValue()
+    {
+        text.text = _value.ToString();
+    }
+
+    public void SetToZero()
+    {
+        _value = 0;
+        UpdateValue();
+    }
+    
+    public void SetValue(int x)
+    {
+        _value = x;
+        UpdateValue();
+
+    }
+
+    public void AddOne()
+    {
+        _value++;
+        UpdateValue();
+
+    }
+
+    public void AddValue(int x)
+    {
+        _value += x;
+        UpdateValue();
+
+    }
+    
+    
+}

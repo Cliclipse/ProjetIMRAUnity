0610e615d6edc5778c1df0c669f3182db6fc85c8
Cliclipse <120763901+Cliclipse@users.noreply.github.com>
2026-05-17 07:43:17 +0200

    .

--- ProjetAugmentedReality/Assets/Plateau.cs
+++ ProjetAugmentedReality/Assets/Plateau.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class Plateau : MonoBehaviour
 {
-    [SerializeField] private Case[] cases; //Elles sont placées selon l'ordre inverse des aiguillies d'une montre en partant d'en haut à gauche
+    [SerializeField]
+    private Case[]
+        cases; //Elles sont placées selon l'ordre inverse des aiguillies d'une montre en partant d'en haut à gauche
 
     public int[] scores;
 
-    public void Semer(int caseChoisie, int joueur) // joueur = 0 ou 1, je traite pas encore l'edgecase du fait que l'autre doit pouvoir jouer
+    public void
+        Semer(int caseChoisie,
+            int joueur) // joueur = 0 ou 1, je traite pas encore l'edgecase du fait que l'autre doit pouvoir jouer
     {
         if (joueur != 0 && joueur != 1)
         {
             Debug.Log("joueur incorrect");
         }
+
         int billeLibre = cases[caseChoisie].getTotalBille();
         cases[caseChoisie].Vider();
 
@@ -21,7 +26,8 @@ public class Plateau : MonoBehaviour
             Case caseActuelle = cases[numeroCase];
             caseActuelle.AddBille();
 
-            if (i == billeLibre && caseActuelle.getTotalBille() == 2 || caseActuelle.getTotalBille() == 3 ) // dernière bille placée
+            if (i == billeLibre && caseActuelle.getTotalBille() == 2 ||
+                caseActuelle.getTotalBille() == 3) // dernière bille placée
             {
                 scores[joueur] += caseActuelle.getTotalBille();
                 caseActuelle.Vider();
